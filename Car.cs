4b65b820e9cdd1ff758dcb005da39c5cdd19a9dd
HlayisaniH <90471779+HlayisaniH@users.noreply.github.com>
2023-09-19 11:42:13 +0200

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicle_Attribute
+{
+    internal class Car
+    {
+
+        public int year;
+        public int carId; 
+        public string carName;
+
+        public static int carCount;
+
+        public Car(string aCarName, int aCarId, int aYear) 
+        {
+            year = aYear;
+            carName = aCarName; 
+            carId = aCarId;
+            carCount++;
+        
+        }    
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicle_Attribute
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car("BMW",98066,2020);
+            Car car2 = new Car("AUDI",09866,2019);
+
+            Console.WriteLine(car1.carName + " " + car2.carName);
+            Console.WriteLine(Car.carCount);
+
+            Console.ReadLine();
+        }
+    }
+}
